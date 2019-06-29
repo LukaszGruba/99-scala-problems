eb8b3fe05d6dcf91afd67e2a3253ab6e4e2501e3
LukaszGruba <lukgru@gmail.com>
2019-06-29 23:51:01 +0200

    P27 - part 2 + refactoring

--- P27.scala
+++ P27.scala
@@ -1,18 +1,16 @@
-type Group = List[String]
+type Group[A] = List[A]
 
-type Combination = List[Group]
+type Combination[A] = List[Group[A]]
 
-def group3(people: List[String]): List[Combination] = {
-  val groupsOf2: List[Group] = combinations(2, people)
-  groupsOf2.flatMap { group2 =>
-    val remaining = people.filterNot(group2.contains)
-    val groupsOf3 = combinations(3, remaining)
-    val groupsOf3And4 =
-      groupsOf3.map { group3 =>
-        val groupOf4 = remaining.filterNot(group3.contains)
-        (group3, groupOf4)
+def group[A](groupDefs: List[Int], elems: List[A]): List[Combination[A]] =
+  groupDefs match {
+    case List(grpDef) => List(List(elems))
+    case grpDef +: defs =>
+      val groups = combinations(grpDef, elems)
+      groups.flatMap { grp =>
+        val remaining = elems.filterNot(grp.contains)
+        group(groupDefs.tail, remaining).map(grp +: _)
       }
-    groupsOf3And4.map { case (g3, g4) => List(group2, g3, g4) }
   }
-}
 
+def group3[A](elems: List[A]): List[Combination[A]] = group(List(2, 3, 4), elems)
